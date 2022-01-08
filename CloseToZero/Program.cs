5d523de56ff1c06d4d5a27f6cf5040338c616d52
Tich <tzvidzie@gmail.com>
2022-01-08 21:15:08 +0200

    initial

---
+++ CloseToZero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CloseToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
