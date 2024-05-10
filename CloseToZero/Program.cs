c37de055809a4d7ca25e8897aa3f664126586a89
Tich Zvidzayi <tzvidzie@gmail.com>
2024-05-10 07:42:35 +0200

    Update Program.cs

--- CloseToZero/Program.cs
+++ CloseToZero/Program.cs
@@ -7,46 +7,31 @@ namespace CloseToZero
     class Program
     {
         static void Main(string[] args)
-        {
-            /*
-             * return the closest number to zero
-             */
-            List<int> xs = new List<int>() {-23,190,4,5,-9,-92,7,89 }; 
+        {   
+            List<int> xs = new List<int>() {-23,190,4,5,-9,-92,7,89 };  //sample input
             Console.WriteLine( "The closest number to zero is " + Closest(xs));
-            Console.Read();
+            Console.ReadLine();
         }
-
+        
         private static int Closest(List<int> xs)
         {
             List<int> neg = new List<int>(), pos = new List<int>();
             foreach (int i in xs)
             {
                 if (i == 0)
-                {
                     return 0;
-                }
                 if (i < 0)
-                {
                     neg.Add(i);
-                }
                 else
-                { 
-                    pos.Add(i);
-                }
+                    pos.Add(i);  
             }
 
             if (neg.Any() == false)
-            { 
                 return pos.Min();
-            }
             else if (pos.Any() == false)
-            { 
                 return neg.Max(); 
-            }
             else
-            {
             return (Math.Abs(neg.Max()) <= pos.Min() ? neg.Max() : pos.Min());
-            }
         }
     }
 }
